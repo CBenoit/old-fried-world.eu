408231703751814364e6a197016723696a498ad6
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-01-19 00:12:15 +0100

    Remove private module.

--- src/config/route.rs
+++ src/config/route.rs
@@ -2,7 +2,6 @@ use router;
 
 use handlers;
 use tests;
-use private;
 
 pub fn configure_routes(router: &mut router::Router) {
     // configure the router
@@ -13,8 +12,5 @@ pub fn configure_routes(router: &mut router::Router) {
 
         .get("/hello_world", tests::hello_world, "hello_world")
         .get("/locales_test/:lang", tests::locales_test, "locales_test");
-
-    // configure private routes
-    private::configure_routes(router);
 }
 
--- src/private/mod.rs
+++
@@ -1,6 +0,0 @@
-use router;
-
-pub fn configure_routes(_: &mut router::Router) {
-    // I put my private routes here.
-}
-
